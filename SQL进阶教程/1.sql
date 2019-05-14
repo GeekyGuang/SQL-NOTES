5d13cdb7bd26b26e529fa9e1a7684946bca5a10d
Travis <travis.x.wong@gmail.com>
2019-05-14 11:53:52 +0800

    chapter 1

--- SQL进阶教程/1.sql
+++ SQL进阶教程/1.sql
@@ -52,9 +52,6 @@ SET p_key = CASE WHEN p_key = 'a' THEN 'b'
 WHERE p_key IN ('a','b')
 
 
-
-
-
 -- 表的匹配
 SELECT course_name,
        CASE WHEN course_id IN (SELECT course_id FROM opencourses
@@ -68,7 +65,7 @@ SELECT course_name,
 							   ELSE 'x' END AS Auguest
 FROM coursemaster;
 
--- 表的匹配用 EXISTS
+-- 表的匹配用 EXISTS: 当这个字段在另一张表里有对应的值的时候,则...
 SELECT course_name,
        CASE WHEN EXISTS (SELECT course_id FROM OpenCourses oc
 	                     WHERE month = '200706' 
@@ -84,6 +81,67 @@ SELECT course_name,
 			ELSE 'x' END AS Auguest
 FROM coursemaster cm
 
+-- Case表达式内使用聚合函数
+SELECT std_id,
+       CASE WHEN COUNT(*) = 1 THEN MAX(club_id)  -- 因为使用了GROUP BY,所以要用MAX
+	   ELSE MAX(CASE WHEN main_club_flg = 'Y' THEN club_id
+	        ELSE NULL END)
+	   END AS main_club
+FROM StudentClub 
+GROUP BY std_id
+
+
+/*********************************************************
+ *                        练习题                         *
+ *********************************************************/
+-- 1. 
+SELECT keys,
+       CASE WHEN x >= y THEN x ELSE y END AS greatest
+FROM greatests
+
+SELECT keys,
+       CASE WHEN x >= y AND x >= z THEN x  -- 同一行中的不同列对比
+	        WHEN y >= x AND y >= z THEN y 
+			ELSE z END AS greatest 
+FROM greatests
+
+SELECT keys,
+       CASE WHEN CASE WHEN x > y THEN x ELSE y END < z THEN z  -- 将CASE表达式的值对比
+	        ELSE CASE WHEN x > y THEN x ELSE y END 
+			END AS greatest
+FROM greatests
+
+-- 如果列数大于3个，先将每一列查询到同一列后用MAX()函数
+SELECT keys, MAX(col) AS greatest
+FROM (SELECT keys, x AS col FROM greatests 
+      UNION ALL 
+	  SELECT keys, y AS col FROM greatests
+	  UNION ALL 
+	  SELECT keys, z AS col FROM greatests) TMP
+GROUP BY keys
+
+-- 2.
+SELECT CASE WHEN sex = 1 THEN '男' 
+            WHEN sex = 2 THEN '女'
+			ELSE NULL END AS '性别',
+	   SUM(population) AS '全国',
+	   SUM(CASE WHEN pref_name = '德岛' THEN population ELSE 0 END) AS '德岛',
+	   SUM(CASE WHEN pref_name = '香川' THEN population ELSE 0 END) AS '香川',
+	   SUM(CASE WHEN pref_name = '爱媛' THEN population ELSE 0 END) AS '爱媛',
+	   SUM(CASE WHEN pref_name = '高知' THEN population ELSE 0 END) AS '高知',
+	   SUM(CASE WHEN pref_name IN ('德岛','香川','爱媛','高知') THEN population ELSE 0 END) AS '四国'
+FROM poptbl2
+GROUP BY sex  -- 先分组，再在分组内按条件求值
+
+
+-- 3.
+SELECT *
+FROM greatests 
+ORDER BY CASE WHEN keys = 'B' THEN 1    -- 在ORDER BY内用CASE改变排序顺序
+              WHEN keys = 'A' THEN 2
+			  WHEN keys = 'D' THEN 3
+			  WHEN keys = 'C' THEN 4
+		 ELSE NULL END
 
 
 
