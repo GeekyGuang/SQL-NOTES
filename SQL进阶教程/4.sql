e7a7b3d6a287e558ab32a89683fe12f795a189e2
Travis <travis.x.wong@gmail.com>
2019-05-18 09:19:32 +0800

    4

--- SQL进阶教程/4.sql
+++ SQL进阶教程/4.sql
@@ -74,10 +74,29 @@ HAVING COUNT(SI.item) = (SELECT COUNT(item) FROM items)
    AND COUNT(I.item) = (SELECT COUNT(item) FROM items)
 
 
-SELECT 
-FROM seqtbl 
+SELECT '存在缺失的编号' AS gap 
+  FROM seqtbl 
+HAVING COUNT(*) <> MAX(seq) 
+UNION ALL 
+SELECT '不存在缺失的编号' AS gap 
+  FROM seqtbl 
+HAVING COUNT(*) = MAX(seq)
 
 
+SELECT CASE WHEN COUNT(*) <> MAX(seq) 
+            THEN '存在缺失的编号'
+			ELSE '不存在缺失的编号' END AS gap 
+FROM seqtbl
 
+SELECT dpt 
+  FROM students 
+GROUP BY dpt 
+HAVING COUNT(*) = SUM(CASE WHEN sbmt_date BETWEEN '2005-09-01' AND '2005-09-30' THEN 1 ELSE 0 END)
 
+SELECT SI.shop,
+       COUNT(SI.item) AS my_item_cnt,
+	   (SELECT COUNT(item) FROM Items) -COUNT(SI.item) AS diff_cnt
+FROM shopItems SI, Items I
+WHERE SI.item = I.item 
+GROUP BY SI.shop
 
